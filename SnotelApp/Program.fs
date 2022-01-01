cfcb245fe4c5bd8467ce2f49daeedaec792303a1
Graham Riches <graham.riches@live.com>
2022-01-01 11:16:06 -0700

    Initial plotting hookup

--- SnotelApp/Program.fs
+++ SnotelApp/Program.fs
@@ -1,112 +1,108 @@
-﻿open System
+﻿namespace SnotelApplication
+
+open System
 open Elmish
 open Elmish.WPF
 open WpfControls
-open SnotelService.Interfaces
 open SnotelService.Responses
 open SnotelService.Parameters
 open OxyPlot
 open OxyPlot.Series
-
-type SiteData = {
-    Temperature: Decimal
-    SnowDepth: Decimal
-    SWE: Decimal
-}
-
-type Entity = {
-    Id: int
-    Value: string
-}
-
-type Model = {
-    Entities: Entity list
-    Selected: int option
-    Service: ISnotelWebClient
-    StationMetadata: StationMetadataResponse option
-    SiteData: SiteData option
-    Plot: PlotModel option
-}
-
-type MessageType =
-  | Refresh
-  | Select of int option
-  | LoadFailed of exn
-  | LoadSuccess of HourlyDataResponse
+open OxyPlot.Axes
 
 
-type PlotDataRequest = {
-    Model: Model
-    ItemId: int option
-}
-
-type CommandMsg =
-    | LoadPlotData of PlotDataRequest
-
-let runAsync task = 
-    task
-    |> Async.AwaitTask
-    |> Async.RunSynchronously
-
-let init() = 
-    let m = { 
-        Entities = [{Id = 0; Value = "Temperature"}; {Id = 1; Value = "Snow Depth"}; {Id = 2; Value = "SWE"}]
-        Selected = None
-        Service = SnotelService.Service.Client
-        StationMetadata = None
-        SiteData = None
-        Plot = None}            
-    let meta = m.Service.GetStationMetadataAsync "787:MT:SNTL" |> runAsync
-    let temperature = m.Service.GetCurrentDataAsync("787:MT:SNTL", ElementType.AirTemperature) |> runAsync
-    let depth = m.Service.GetCurrentDataAsync("787:MT:SNTL", ElementType.SnowDepth) |> runAsync
-    let swe = m.Service.GetCurrentDataAsync("787:MT:SNTL", ElementType.SnowWaterEquivalent) |> runAsync
-    let data = {Temperature = temperature.Value; SnowDepth = depth.Value; SWE = swe.Value}    
-    {m with StationMetadata = Some meta; SiteData = Some data}, []
-
-let load request =
-    async {
-        let! response = request.Model.Service.GetHourlyDataAsync("787:MT:SNTL", ElementType.AirTemperature, DateTime.Now) |> Async.AwaitTask
-        return LoadSuccess response
+module Core = 
+    type Entity = {
+        Id: int
+        Value: string
     }
 
-let createNewPlotModel seriesData = 
-    let plot = PlotModel()
-    do
-        let series = LineSeries()
-        series.Points.Add(new DataPoint(0, 0))
-        series.Points.Add(new DataPoint(1, 1))
-        plot.Series.Add(series)
-    Some plot
+    type Model = {
+        Site: SnotelSite.Snotel option
+        Entities: Entity list
+        Selected: int option    
+        Plot: PlotModel option
+    }
 
-let update msg model =
-    match msg with 
-    | Refresh -> model, []
-    | Select selected -> model, [LoadPlotData {Model = model; ItemId = selected }]
-    | LoadFailed e -> model, []
-    | LoadSuccess data -> {model with Plot = createNewPlotModel data}, []
+    type MessageType =
+      | Select of int option
+      | LoadSiteDataSuccess of SnotelSite.Snotel
+      | LoadSiteDataFailed of exn
+      | LoadMetricsFailed of exn
+      | LoadMetricsSuccess of HourlyDataResponse
+      | Empty
 
-let toCmd = function
-   | LoadPlotData request -> Cmd.OfAsync.either load request id LoadFailed
+    type CommandMsg =
+        | LoadStationData of string
+        | LoadPlotData of SnotelSite.DataRequest
 
-let bindings () : Binding<Model, MessageType> list = [
-    "SiteName"     |> Binding.oneWay (fun m -> if m.StationMetadata.IsSome then m.StationMetadata.Value.Name else "")
-    "State"        |> Binding.oneWay (fun m -> if m.StationMetadata.IsSome then m.StationMetadata.Value.State else "")
-    "Coordinates"  |> Binding.oneWay (fun m -> if m.StationMetadata.IsSome then m.StationMetadata.Value.Location else "")
-    "Elevation"    |> Binding.oneWay (fun m -> if m.StationMetadata.IsSome then string m.StationMetadata.Value.Elevation else "")
-    "Temperature"  |> Binding.oneWay (fun m -> if m.SiteData.IsSome then string m.SiteData.Value.Temperature else "")
-    "SnowDepth"    |> Binding.oneWay (fun m -> if m.SiteData.IsSome then string m.SiteData.Value.SnowDepth else "")
-    "SWE"          |> Binding.oneWay (fun m -> if m.SiteData.IsSome then string m.SiteData.Value.SWE else "")
-    "Plot"         |> Binding.oneWay (fun m -> if m.Plot.IsSome then m.Plot.Value else PlotModel())
-    "PlotItems"    |> Binding.subModelSeq
-        (fun m -> m.Entities
-        , fun e -> e.Id
-        , fun () -> [
-        "Name" |> Binding.oneWay (fun (_, e) -> e.Value)
-        ])
-    "ItemSelected" |> Binding.subModelSelectedItem("PlotItems", (fun m -> m.Selected), Select)
-]
+    let init () = 
+        let m = {
+            Site = None
+            Entities = [{Id = 0; Value = "Temperature"}; {Id = 1; Value = "Snow Depth"}; {Id = 2; Value = "SWE"}]
+            Selected = None
+            Plot = None}            
+        m, [LoadStationData "787:MT:SNTL"]
 
-[<EntryPoint; STAThread>]
-let main _ =
-  Program.mkProgramWpfWithCmdMsg init update bindings toCmd
-  |> Program.runWindow (MainView())
+    let loadSiteFromString site =
+        async {
+            let! site = SnotelSite.loadSiteFromStationTriplet site
+            return LoadSiteDataSuccess site
+        }
+
+    let loadDataMetrics request = 
+        async {
+            let! response = SnotelSite.handleDataRequest request
+            match response with
+            | Some r -> return LoadMetricsSuccess r
+            | None -> return Empty      
+        }
+
+    let createNewPlotModel (seriesData: HourlyDataResponse) = 
+        let plot = PlotModel()
+        do
+            let series = LineSeries(Color=OxyColors.Black)
+            plot.Axes.Add(new DateTimeAxis(StringFormat="MM-dd HH:mm", Position=AxisPosition.Bottom))
+            for dataPoint in seriesData.DataPoints do
+                series.Points.Add(new DataPoint(DateTimeAxis.ToDouble(DateTime.Parse dataPoint.Label), float dataPoint.Value))
+            plot.Series.Add(series)
+        Some plot
+
+    let update msg model =
+        match msg with 
+        | Select selected -> model, [LoadPlotData {Site = model.Site; ItemId = selected }]
+        | LoadSiteDataSuccess site -> { model with Site = Some site }, []        
+        | LoadMetricsSuccess data -> {model with Plot = createNewPlotModel data}, []        
+        | _ -> model, []
+
+    let toCmd = function
+        | LoadStationData site -> Cmd.OfAsync.either loadSiteFromString site id LoadSiteDataFailed
+        | LoadPlotData request -> Cmd.OfAsync.either loadDataMetrics request id LoadMetricsFailed
+
+    let getTextFieldFromOption optValue = 
+        match optValue with
+        | Some v -> v
+        | None -> ""
+
+    let bindings () : Binding<Model, MessageType> list = [
+        "SiteName"     |> Binding.oneWay (fun m -> if m.Site.IsSome then m.Site.Value.MetaData.Name else "")
+        "State"        |> Binding.oneWay (fun m -> if m.Site.IsSome then m.Site.Value.MetaData.State else "")
+        "Coordinates"  |> Binding.oneWay (fun m -> if m.Site.IsSome then m.Site.Value.MetaData.Location else "")
+        "Elevation"    |> Binding.oneWay (fun m -> if m.Site.IsSome then string m.Site.Value.MetaData.Elevation else "")
+        "Temperature"  |> Binding.oneWay (fun m -> if m.Site.IsSome then string m.Site.Value.SiteData.Temperature else "")
+        "SnowDepth"    |> Binding.oneWay (fun m -> if m.Site.IsSome then string m.Site.Value.SiteData.SnowDepth else "")
+        "SWE"          |> Binding.oneWay (fun m -> if m.Site.IsSome then string m.Site.Value.SiteData.SWE else "")
+        "Plot"         |> Binding.oneWay (fun m -> if m.Plot.IsSome then m.Plot.Value else PlotModel())
+        "PlotItems"    |> Binding.subModelSeq
+            (fun m -> m.Entities
+            , fun e -> e.Id
+            , fun () -> [
+            "Name" |> Binding.oneWay (fun (_, e) -> e.Value)
+            ])
+        "ItemSelected" |> Binding.subModelSelectedItem("PlotItems", (fun m -> m.Selected), Select)
+    ]
+
+    [<EntryPoint; STAThread>]
+    let main _ =
+      Program.mkProgramWpfWithCmdMsg init update bindings toCmd
+      |> Program.runWindow (MainView())
---
+++ SnotelApp/SnotelSite.fs
@@ -0,0 +1,49 @@
+﻿namespace SnotelApplication
+
+open System
+open SnotelService.Responses
+open SnotelService.Interfaces
+open SnotelService.Parameters
+
+module SnotelSite = 
+    type SiteData = {
+        Temperature: Decimal
+        SnowDepth: Decimal
+        SWE: Decimal
+    }
+
+    type Snotel = {
+        Service: ISnotelWebClient
+        StationTriplet: string
+        MetaData: StationMetadataResponse
+        SiteData: SiteData
+    }
+
+    type DataRequest = {
+        Site: Snotel option
+        ItemId: int option
+    }    
+
+    let handleDataRequest request =
+        async {
+            match request.Site with
+            | Some s -> 
+                let! data = s.Service.GetHourlyDataAsync(s.StationTriplet, ElementType.AirTemperature, DateTime.Now.AddDays(-1)) |> Async.AwaitTask
+                return Some data
+            | None -> return None
+        }                    
+
+    let loadSiteFromStationTriplet stationTriplet = 
+        async {
+            let service = SnotelService.Service.Client
+            let! meta = service.GetStationMetadataAsync stationTriplet |> Async.AwaitTask
+            let! temperature = service.GetCurrentDataAsync(stationTriplet, ElementType.AirTemperature) |> Async.AwaitTask
+            let! depth = service.GetCurrentDataAsync(stationTriplet, ElementType.SnowDepth) |> Async.AwaitTask
+            let! swe = service.GetCurrentDataAsync(stationTriplet, ElementType.SnowWaterEquivalent) |> Async.AwaitTask            
+            let data = {Temperature = temperature.Value; SnowDepth = depth.Value; SWE = swe.Value}
+            return {
+                Service = service
+                StationTriplet = stationTriplet
+                MetaData = meta
+                SiteData = data }
+        }
